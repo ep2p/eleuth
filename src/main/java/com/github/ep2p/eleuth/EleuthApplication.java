d3bbad023ef1a0d6fbc431b1d014f8f06aac6e2a
sepehr-gh <sepehrgh01@gmail.com>
2020-11-27 16:10:29 +0330

    Init

---
+++ src/main/java/com/github/ep2p/eleuth/EleuthApplication.java
@@ -0,0 +1,13 @@
+package com.github.ep2p.eleuth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EleuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EleuthApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/ep2p/eleuth/EleuthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.ep2p.eleuth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
