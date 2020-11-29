eaf50125616586492bf2811e2539bbfff65a6c34
sepehr-gh <sepehrgh01@gmail.com>
2020-11-29 11:36:39 +0330

    feat: added HTTPS support using generated keys

--- dockerRun.sh
+++ dockerRun.sh
@@ -2,4 +2,4 @@
 mvn clean package -DskipTests
 echo $(pwd)
 sudo docker build -t "ep2p/eleuth:test_1" .
-sudo docker run -v $(pwd)/workingDir:/home/ -e ARGS="--config.nodeType=RING --config.workingDir=/home/ --config.cn=myhost.com" "ep2p/eleuth:test_1"
+sudo docker run -v $(pwd)/workingDir:/home/ -e ARGS="--config.nodeType=RING --config.workingDir=/home --config.cn=myhost.com --spring.profiles.active=ssl" "ep2p/eleuth:test_1"
