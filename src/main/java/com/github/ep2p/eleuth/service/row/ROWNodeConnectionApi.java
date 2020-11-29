807010776dcebdb6af1443c67c3b14823d808f8f
sepehr-gh <sepehrgh01@gmail.com>
2020-11-29 11:05:15 +0330

    fix: ROWNodeConnectionApi now implements correct methods from super class

--- src/main/java/com/github/ep2p/eleuth/service/row/ROWNodeConnectionApi.java
+++ src/main/java/com/github/ep2p/eleuth/service/row/ROWNodeConnectionApi.java
@@ -8,9 +8,11 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.math.BigInteger;
+
 @Service("rowNodeConnectionApi")
 @Slf4j
-public class ROWNodeConnectionApi implements NodeConnectionApi<ROWConnectionInfo> {
+public class ROWNodeConnectionApi implements NodeConnectionApi<BigInteger, ROWConnectionInfo> {
     private final RowConnectionPool rowConnectionPool;
 
     @Autowired
@@ -19,38 +21,38 @@ public class ROWNodeConnectionApi implements NodeConnectionApi<ROWConnectionInfo
     }
 
     @Override
-    public PingAnswer ping(Node<ROWConnectionInfo> caller, Node<ROWConnectionInfo> node) {
+    public PingAnswer ping(Node<BigInteger, ROWConnectionInfo> caller, Node<BigInteger, ROWConnectionInfo> node) {
         return null;
     }
 
     @Override
-    public void shutdownSignal(Node<ROWConnectionInfo> caller, Node<ROWConnectionInfo> node) {
+    public void shutdownSignal(Node<BigInteger, ROWConnectionInfo> caller, Node<BigInteger, ROWConnectionInfo> node) {
         return;
     }
 
     @Override
-    public FindNodeAnswer<ROWConnectionInfo> findNode(Node<ROWConnectionInfo> caller, Node<ROWConnectionInfo> node, Integer nodeId) {
+    public FindNodeAnswer<BigInteger, ROWConnectionInfo> findNode(Node<BigInteger, ROWConnectionInfo> caller, Node<BigInteger, ROWConnectionInfo> node, BigInteger nodeId) {
         return null;
     }
 
     //not async yet
     @Override
-    public <K, V> void storeAsync(Node<ROWConnectionInfo> caller, Node<ROWConnectionInfo> requester, Node<ROWConnectionInfo> node, K key, V value) {
+    public <K, V> void storeAsync(Node<BigInteger, ROWConnectionInfo> caller, Node<BigInteger, ROWConnectionInfo> requester, Node<BigInteger, ROWConnectionInfo> node, K key, V value) {
         return;
     }
 
     @Override
-    public <K> void getRequest(Node<ROWConnectionInfo> caller, Node<ROWConnectionInfo> requester, Node<ROWConnectionInfo> node, K key) {
+    public <K> void getRequest(Node<BigInteger, ROWConnectionInfo> caller, Node<BigInteger, ROWConnectionInfo> requester, Node<BigInteger, ROWConnectionInfo> node, K key) {
         return;
     }
 
     @Override
-    public <K, V> void sendGetResults(Node<ROWConnectionInfo> caller, Node<ROWConnectionInfo> requester, K key, V value) {
+    public <K, V> void sendGetResults(Node<BigInteger, ROWConnectionInfo> caller, Node<BigInteger, ROWConnectionInfo> requester, K key, V value) {
         return;
     }
 
     @Override
-    public <K> void sendStoreResults(Node<ROWConnectionInfo> caller, Node<ROWConnectionInfo> requester, K key, boolean success) {
+    public <K> void sendStoreResults(Node<BigInteger, ROWConnectionInfo> caller, Node<BigInteger, ROWConnectionInfo> requester, K key, boolean success) {
         return;
     }
 
