da69dae40ccdeefa06fb7a54df3ebdb088d8e2f4
sepehr-gh <sepehrgh01@gmail.com>
2020-11-29 10:57:02 +0330

    fix: ExternalNodeSerializer has been fixed

--- src/main/java/com/github/ep2p/eleuth/config/EleuthConfiguration.java
+++ src/main/java/com/github/ep2p/eleuth/config/EleuthConfiguration.java
@@ -11,7 +11,7 @@ import com.github.ep2p.encore.helper.KeyStoreWrapper;
 import com.github.ep2p.encore.key.CNGenerator;
 import com.github.ep2p.encore.key.PubHashUserId128Generator;
 import com.github.ep2p.encore.key.UserIdGenerator;
-import com.github.ep2p.kademlia.node.ExternalNode;
+import com.github.ep2p.kademlia.node.external.ExternalNode;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
--- src/main/java/com/github/ep2p/eleuth/config/ExternalNodeSerializer.java
+++ src/main/java/com/github/ep2p/eleuth/config/ExternalNodeSerializer.java
@@ -3,14 +3,16 @@ package com.github.ep2p.eleuth.config;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
-import com.github.ep2p.kademlia.node.ExternalNode;
+import com.github.ep2p.eleuth.service.row.ROWConnectionInfo;
+import com.github.ep2p.kademlia.node.external.ExternalNode;
 
 import java.io.IOException;
+import java.math.BigInteger;
 
-public class ExternalNodeSerializer extends JsonSerializer<ExternalNode> {
+public class ExternalNodeSerializer extends JsonSerializer<ExternalNode<BigInteger, ROWConnectionInfo>> {
 
     @Override
-    public void serialize(ExternalNode externalNode, JsonGenerator jgen, SerializerProvider serializerProvider) throws IOException {
+    public void serialize(ExternalNode<BigInteger, ROWConnectionInfo> externalNode, JsonGenerator jgen, SerializerProvider serializerProvider) throws IOException {
         jgen.writeStartObject();
         jgen.writeNumberField("id", externalNode.getId());
         jgen.writeNumberField("distance", externalNode.getDistance());
