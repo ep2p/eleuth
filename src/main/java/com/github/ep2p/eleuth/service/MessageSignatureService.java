93a219fa6d7b63c80a82bf3f0de8bf185e381e88
sepehr-gh <sepehrgh01@gmail.com>
2020-12-02 12:56:09 +0330

    fix: using json string to serialize

--- src/main/java/com/github/ep2p/eleuth/service/MessageSignatureService.java
+++ src/main/java/com/github/ep2p/eleuth/service/MessageSignatureService.java
@@ -1,5 +1,6 @@
 package com.github.ep2p.eleuth.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.ep2p.eleuth.exception.InvalidSignatureException;
 import com.github.ep2p.eleuth.model.dto.SignedData;
 import com.github.ep2p.eleuth.node.NodeInformation;
@@ -21,24 +22,27 @@ import java.security.PublicKey;
 public class MessageSignatureService {
     private final MessageSigner messageSigner;
     private final NodeInformation nodeInformation;
+    private final ObjectMapper objectMapper;
     private final BytesPublicKeyGenerator bytesPublicKeyGenerator = new BytesPublicKeyGenerator();
 
-    public MessageSignatureService(NodeInformation nodeInformation) {
+    public MessageSignatureService(NodeInformation nodeInformation, ObjectMapper objectMapper) {
         this.nodeInformation = nodeInformation;
         messageSigner = new MessageSigner(nodeInformation.getKeyPair().getPrivate());
+        this.objectMapper = objectMapper;
     }
 
     @SneakyThrows
     public <E extends Serializable> SignedData<E> sign(E input, boolean includePublicKey){
-        Serializer<E> serializer = new Serializer<>();
-        byte[] serializedData = serializer.serialize(input);
+        Serializer<String> serializer = new Serializer<>();
+        String valueAsString = objectMapper.writeValueAsString(input);
+        byte[] serializedData = serializer.serialize(valueAsString);
         byte[] sign = messageSigner.sign(serializedData);
         return new SignedData<E>(input, includePublicKey ? Base64Util.encode(nodeInformation.getKeyPair().getPublic().getEncoded()) : null, Base64Util.encode(sign));
     }
 
     public <E extends Serializable> void validate(SignedData<E> signedData) throws InvalidSignatureException {
         Assert.notNull(signedData.getPublicKey(), "Public key can not be null");
-        this.validate(signedData, bytesPublicKeyGenerator.generate(Base64Util.decode(signedData.getPublicKey())));
+        this.validate(signedData, signedData.getPublicKey());
     }
 
     public <E extends Serializable> void validate(SignedData<E> signedData, String publicKey) throws InvalidSignatureException {
@@ -48,9 +52,10 @@ public class MessageSignatureService {
 
     public <E extends Serializable> void validate(SignedData<E> signedData, PublicKey publicKey) throws InvalidSignatureException {
         Assert.notNull(signedData.getSignature(), "Signature can not be null");
-        Serializer<E> serializer = new Serializer<>();
+        Serializer<String> serializer = new Serializer<>();
         try {
-            byte[] serializedData = serializer.serialize(signedData.getData());
+            String valueAsString = objectMapper.writeValueAsString(signedData.getData());
+            byte[] serializedData = serializer.serialize(valueAsString);
             byte[] signature = Base64Util.decode(signedData.getSignature());
             if (!SignatureVerifier.verify(publicKey, signature, serializedData)) {
                 throw new InvalidSignatureException();
