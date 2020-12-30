93ee0847a2e1717efa1379e3e9f62ce459ed2267
sepehr-gh <sepehrgh01@gmail.com>
2020-12-30 11:12:40 +0330

    fix: using RouteMessageSender in AvailabilityPublishListener

--- src/main/java/com/github/ep2p/eleuth/service/listener/AvailabilityPublishListener.java
+++ src/main/java/com/github/ep2p/eleuth/service/listener/AvailabilityPublishListener.java
@@ -1,53 +1,31 @@
 package com.github.ep2p.eleuth.service.listener;
 
-import com.github.ep2p.eleuth.model.dto.route.AvailabilityMessage;
-import com.github.ep2p.eleuth.model.dto.route.AvailabilityReply;
 import com.github.ep2p.eleuth.model.event.AvailabilityPublishEvent;
+import com.github.ep2p.eleuth.service.route.RouteMessageSender;
+import com.github.ep2p.eleuth.service.row.ROWConnectionInfo;
 import com.github.ep2p.eleuth.service.row.RowConnectionPool;
-import lab.idioglossia.row.client.RowClient;
-import lab.idioglossia.row.client.callback.ResponseCallback;
-import lab.idioglossia.row.client.model.RowRequest;
-import lab.idioglossia.row.client.model.RowResponse;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationListener;
 import org.springframework.stereotype.Service;
 
-import java.io.IOException;
-
 //listens to "AvailabilityPublishEvent" and forwards Availability message to other nodes
 @Service
 @Slf4j
 public class AvailabilityPublishListener implements ApplicationListener<AvailabilityPublishEvent> {
-    private final RowConnectionPool rowConnectionPool;
+    private final RouteMessageSender<ROWConnectionInfo> routeMessageSender;
 
     @Autowired
-    public AvailabilityPublishListener(RowConnectionPool rowConnectionPool) {
-        this.rowConnectionPool = rowConnectionPool;
+    public AvailabilityPublishListener(RouteMessageSender<ROWConnectionInfo> routeMessageSender) {
+        this.routeMessageSender = routeMessageSender;
     }
 
     @Override
     public void onApplicationEvent(AvailabilityPublishEvent availabilityPublishEvent) {
-        ResponseCallback<AvailabilityReply> callback = new ResponseCallback<AvailabilityReply>(AvailabilityReply.class) {
-            @Override
-            public void onResponse(RowResponse<AvailabilityReply> rowResponse) {
-                log.trace(rowResponse.toString());
-            }
-
-            @Override
-            public void onError(Throwable throwable) {
-                log.error("A node failed to handle availability request", throwable);
-            }
-        };
         availabilityPublishEvent.getNodesToContact().forEach(nodeConnectionEntity -> {
-            RowClient client = rowConnectionPool.getClient(nodeConnectionEntity.getNodeId().toString(), nodeConnectionEntity);
             try {
-                client.sendRequest(RowRequest.<AvailabilityMessage, Void>builder()
-                        .body(availabilityPublishEvent.getAvailabilityMessage())
-                        .address("") //todo: set address
-                        .method(RowRequest.RowMethod.POST)
-                        .build(), callback);
-            } catch (IOException e) {
+                routeMessageSender.sendAvailabilityMessage(nodeConnectionEntity.getNodeId().toString(), nodeConnectionEntity, availabilityPublishEvent.getAvailabilityMessage());
+            } catch (Exception e) {
                 log.error("Failed to send request", e);
             }
         });
